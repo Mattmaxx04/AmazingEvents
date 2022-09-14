9f04024e25725b9945dada29185f54361582b98d
Matias <matiasquiroga.04@gmail.com>
2022-09-14 19:15:41 -0300

    update

--- js/upcoming.js
+++ js/upcoming.js
@@ -1,22 +1,5 @@
 console.log(data);
 
-
-let cards = document.getElementById('cards__data');
-
-for(let i = 0;i<data.eventos.length;i++){
-    let card = document.createElement('div');
-    card.className = "card";
-    card.style.width = '18rem';
-    card.innerHTML = `<img src="${data.eventos[i].image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${data.eventos[i].name}</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>`
-    cards.appendChild(card);
-}
-
-/*
 let updata = document.getElementById('updata');
 
 for(let i = 0;i<data.eventos.length;i++){
@@ -32,7 +15,7 @@ for(let i = 0;i<data.eventos.length;i++){
   if(data.eventos[i].date > data.fechaActual){
   updata.appendChild(card);}
 }
-*/
+
 
 /* <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
