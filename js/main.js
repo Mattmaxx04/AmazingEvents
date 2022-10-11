57013fceedc0d7dadd8e2d277524f015f35ef521
Mattmaxx04 <95719090+Mattmaxx04@users.noreply.github.com>
2022-10-11 19:24:29 -0300

    Update JS
    Details move to main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ window.addEventListener("scroll", () => {
   } else {
     toTop.classList.remove("active");
   }
-})
+});
 /*-------------------cards-----------------*/
 let eventosFinal = [];
 const URI = "https://amazing-events.herokuapp.com/api/events";
@@ -15,41 +15,36 @@ traerDatos(URI);
 
 let cards = document.getElementById("cards__data");
 let article = document.getElementById("notfound");
-let div = document.createElement("div") 
+let div = document.createElement("div");
 
-function notFound(){  
-  div.className = "notFound"
+function notFound() {
+  div.className = "notFound";
   div.innerHTML = `<img src="./assets/notfound.png" class="img__notfound" alt="...">`;
   article.appendChild(div);
 }
 
-function pintarCards(events) {   
-  cards.innerHTML = "";   
-  if (!events.length){
-    notFound()
-  }else{  
-    div.innerHTML = ""
-    events.forEach((eventos) => {   
-     
-    let card = document.createElement("div");
-    card.className = "card";
-    card.style.width = "21rem";
-    card.style.padding = "0.5rem";
-    card.innerHTML = `<img src="${eventos.image}" class="card-img-top card__image" alt="...">
+function pintarCards(events) {
+  cards.innerHTML = "";
+  if (!events.length) {
+    notFound();
+  } else {
+    div.innerHTML = "";
+    events.forEach((eventos) => {
+      let card = document.createElement("div");
+      card.className = "card";
+      card.style.width = "21rem";
+      card.style.padding = "0.5rem";
+      card.innerHTML = `<img src="${eventos.image}" class="card-img-top card__image" alt="...">
     <div class="card-body">
       <h5 class="card-title">${eventos.name}</h5>
       <p class="card-text">${eventos.description}</p>
       <a href="./details.html?_id=${eventos._id}" class="btn btn-primary">More info</a>
     </div>`;
-    cards.appendChild(card);
-
-  });
+      cards.appendChild(card);
+    });
+  }
 }
-}
-/*-------------------funciones filtrar por fecha-----------------*/
 
-
-  
 /*-------------------pintar on page-----------------*/
 function traerDatos(url) {
   fetch(url)
@@ -57,24 +52,41 @@ function traerDatos(url) {
     .then((data) => {
       eventosFinal = data.events;
       if (document.title == "Home") {
-        eventosFinal = data.events
+        eventosFinal = data.events;
       } else if (document.title == "Upcoming Events") {
-        eventosFinal = eventosFinal.filter(event => event.date >= data.currentDate);    
-      } else if(document.title == "Past Events") {
-        eventosFinal = eventosFinal.filter(event => event.date < data.currentDate);        
+        eventosFinal = eventosFinal.filter(
+          (event) => event.date >= data.currentDate
+        );
+      } else if (document.title == "Past Events") {
+        eventosFinal = eventosFinal.filter(
+          (event) => event.date < data.currentDate
+        );
+      } else if (document.title == "Details") {
+        let parametrosUrl = location.search;
+        let parametros = new URLSearchParams(parametrosUrl);
+        let id = parametros.get("_id");
+        let eventoFiltrado = eventosFinal.filter(events => {
+          return events._id;
+        });
+        let eventoEncontrado = eventoFiltrado.find(
+          (events) => events._id == id
+        );
+
+      showCard(eventoEncontrado);
+      }else if(document.title == "Stats"){
+        showTable()
       }
-      pintarChecks(eventosFinal)
-      pintarCards(eventosFinal)
+      pintarChecks(eventosFinal);
+      pintarCards(eventosFinal);
     });
 }
-
 
 /*-------------------funcion de busqueda-----------------*/
 
 let searcher = document.getElementById("searchbox");
 
 searcher.addEventListener("keyup", () => {
-  superFiltro()
+  superFiltro();
 });
 
 function search(events) {
@@ -85,12 +97,9 @@ function search(events) {
       eventoFiltrado.push(events);
     }
   });
-  
+
   return eventoFiltrado;
 }
-
-
-
 
 /*-------------------mostrar checkbox por categoria-----------------*/
 
@@ -117,13 +126,12 @@ function pintarChecks(events) {
   });
 }
 
-
 /*-------------------filtro por checkbox-----------------*/
 
 let switches = document.querySelector("form");
 
 switches.addEventListener("click", () => {
-  superFiltro()
+  superFiltro();
 });
 
 function filtrarPorCategoria(events) {
@@ -145,12 +153,185 @@ function filtrarPorCategoria(events) {
   return filtro;
 }
 /*-------------------Funcion super filtro-----------------*/
-function superFiltro(){
+function superFiltro() {
   let filtroCategoria = filtrarPorCategoria(eventosFinal);
-  let filtradoTexto = search(filtroCategoria); 
-  
-  pintarCards(filtradoTexto)
+  let filtradoTexto = search(filtroCategoria);
 
+  pintarCards(filtradoTexto);
 }
 
+/*--------------Funcion show card details-----------------*/
+
+function showCard(events) {
+  let people = assistanceOrEstimate();
+
+  function assistanceOrEstimate() {
+    if (events.assistance == undefined) {
+      return `Estimate: ${events.estimate}`;
+    } else {
+      return `Assistance: ${events.assistance}`;
+    }
+  }
+  let container = document.getElementById("details");
+  container.innerHTML = "";
+  let div = document.createElement("div");
+  div.className = "card";
+  div.style.maxwidth = "80%";
+  div.style.minHeight = "20rem";
+  div.style.padding = "0.5rem";
+  div.innerHTML = `<div class="detail__card">
+    <div class="col-md-6">
+      <img src="${events.image}" class="img-fluid detail__image" alt="eventimage">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h5 class="card-title"> ${events.name}</h5>
+        <p class="card-text detail__p"> ${events.description}</p>
+        <p class="card-text detail__p">Date: ${events.date}</p>
+        <p class="card-text detail__p">Place: ${events.place}</p>
+        <p class="card-text detail__p">Capacity: ${events.capacity}</p>
+        <p class="card-text detail__p">${people}</p>
+        <h4 class="price">Price: $${events.price}</p>
+      </div>
+    </div>
+  </div>`;
+  container.appendChild(div);
+}
+
+
+/*--------------Funcion table-----------------*/
+let eventHighest;
+let eventLowest;
+let eventLarger;
+let categories;
+let revenues;
+let percentage;
+let pastCategories;
+let pastRevenues;
+let pastPercentage;
+
+
+function showTable(){
+  let table = document.getElementById("stats");
+  table.innerHTML = ""
+  let tableBody = document.createElement("table");
+  div.className = "table";
+  tableBody.innerHTML=`<thead>
+  <tr>
+    <th scope="col">Event with the highest percentage of attendance</th>
+ 
+    <th scope="col">Event with the lowest percentage of attendance</th>
+  
+    <th scope="col">Event with larger capacity</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>${eventHighest}</td>
+    <td>${eventLowest}</td>
+    <td>${eventLarger}</td>
+  </tr>            
+</tbody>
+</table>
+<h2>Upcoming Events Statistic by category </h2>
+<table class="tg">
+<thead>
+  <tr>
+    <th scope="col">Categories</th>
+  
+    <th scope="col">Revenues</th>
+  
+    <th scope="col">Percentage of attendance</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>${categories}</td>
+    <td>${revenues}</td>
+    <td>${percentage}</td>
+  </tr>            
+</tbody>
+</table>
+<h2>Past Events Statistic by category </h2>
+<table class="tg">
+
+<thead>
+  <tr>
+    <th scope="col">Categories</th>
+  
+    <th scope="col">Revenues</th>
+  
+    <th scope="col">Percentage of attendance</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>${pastCategories}</td>
+    <td>${pastRevenues}</td>
+    <td>${pastPercentage}</td>
+  </tr>            
+</tbody>
+  `
+  table.appendChild(tableBody);  
+}
+
+/* 
+<thead>
+            <tr>
+              <th scope="col">Events with the highest percentage of attendance</th>
+           
+              <th scope="col">Events with the lowest percentage of attendance</th>
+            
+              <th scope="col">Events with larger capacity</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> </td>
+              <td></td>
+              <td></td>
+            </tr>            
+          </tbody>
+        </table>
+        <h2>Upcoming Events Statistic by category </h2>
+        <table class="tg">
+          
+          <thead>
+            <tr>
+              <th scope="col">Categories</th>
+            
+              <th scope="col">Revenues</th>
+            
+              <th scope="col">Percentage of attendance</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>            
+          </tbody>
+        </table>
+        <h2>Past Events Statistic by category </h2>
+        <table class="tg">
+          
+          <thead>
+            <tr>
+              <th scope="col">Categories</th>
+            
+              <th scope="col">Revenues</th>
+            
+              <th scope="col">Percentage of attendance</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>            
+          </tbody>
+        
+          */
 
